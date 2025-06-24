c5eccfc19f59ddcd27bc7e47a31cee2f4f4d6a92
saraxp <sara.ap408@gmail.com>
2025-06-24 17:56:20 +0530

    UI_changes

--- index.js
+++ index.js
@@ -5,10 +5,30 @@ let currentOperator = '';
 let displayOperator = '';
 let result = 0;
 let precedence = ['/', '*', '+', '-']
+const cursor = document.getElementById('cursor');
+const expression = document.getElementById('expression');
 
+function formatDisplay(expArr) {
+    return expArr
+        .map(token => {
+            if (token === "*") {
+                return `<span class="material-symbols-outlined">close</span>`;
+            }
+            if (token === "/") {
+                return `<svg class="divide" width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 12H20M13 6C13 6.55228 12.5523 7 12 7C11.4477 7 11 6.55228 11 6C11 5.44772 11.4477 5 12 5C12.5523 5 13 5.44772 13 6ZM13 18C13 18.5523 12.5523 19 12 19C11.4477 19 11 18.5523 11 18C11 17.4477 11.4477 17 12 17C12.5523 17 13 17.4477 13 18Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
+            }
+            return token;
+        })
+        .join('');
+}
+
+function updateDisplay(value) {
+    expression.innerHTML = formatDisplay(value);
+    cursor.style.display = value.length > 0 ? 'inline' : 'none';
+}
 function callNumber(input) {
     currentInput += input;
-    document.getElementById('result').value = `${oldInput}${displayOperator}${currentInput}`;
+    updateDisplay([...exp, currentInput]);
 }
 
 function callOperator(operation) {
@@ -20,15 +40,14 @@ function callOperator(operation) {
     displayOperator = operation;
     exp.push(currentOperator);
 
-    // Show the operator before clearing it
-    document.getElementById('result').value = `${oldInput}${displayOperator}${currentInput}`;
+   updateDisplay([...exp]); 
 
     currentOperator = ''; 
     console.log(exp);
 }
 
 function clearDisplay (){
-    document.getElementById('result').value = '';
+    expression.textContent = '';
     exp = [];
     currentInput = '';
     oldInput = '';
@@ -72,5 +91,5 @@ function calculate() {
         }
             
     }
-    document.getElementById('result').value = `${exp[0]}`;
+    updateDisplay([exp[0]]);
 }
